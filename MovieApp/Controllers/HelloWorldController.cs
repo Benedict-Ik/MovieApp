23ccc740bb0eb68bf04801308c186e6d31d9b51f
Benedict Odoh <benedict.ik98@gmail.com>
2024-02-06 11:22:21 +0100

    Newly created repo

---
+++ MovieApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        // Index Route
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Welcome Route
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
